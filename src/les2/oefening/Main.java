7d6bd9bacd3427d5fe8b65fa7961bed6a70a3225
ruben <rubenvanrooijen23@outlook.com>
2021-02-11 15:17:43 +0100

    oefening in de les

--- src/les2/oefening/Main.java
+++ src/les2/oefening/Main.java
@@ -5,7 +5,7 @@ public class Main {
         Rekening r1 = new Rekening(43242234);
         Klant k1 = new Klant("Ruben van Rooijen", "jo", "Spakenburg");
         r1.stort(123.45);
-        r1.setRekeningHouder(k1);
-        System.out.println(r1.toString());
+        k1.setMijnRekening(r1);
+        System.out.println(k1.toString());
     }
 }
--- src/les2/oefening/Rekening.java
+++ src/les2/oefening/Rekening.java
@@ -3,18 +3,9 @@ package les2.oefening;
 public class Rekening {
     private int nummer;
     private Double saldo = 0.0;
-    private Klant rekeninghouder;
 
     public Rekening(Integer nummer){
         this.nummer = nummer;
-    }
-
-    public void setRekeningHouder(Klant rekeninghouder){
-        this.rekeninghouder = rekeninghouder;
-    }
-
-    public Klant getRekeninghouder() {
-        return this.rekeninghouder;
     }
 
     public int getNummer(){
@@ -34,14 +25,7 @@ public class Rekening {
     }
 
     public String toString(){
-        String rekeninginfo = "Op rekening " + nummer + " is het saldo: " + saldo;
-        String rekeninghouderinfo = "";
-        try{
-            rekeninghouderinfo += "Klantnaam: " + this.rekeninghouder.getNaam();
-        }catch (Exception e){
-            rekeninghouderinfo += "De rekeninghouder is onbekend";
-        }
-        return rekeninginfo + "\n" + rekeninghouderinfo;
+        return "Op rekening " + nummer + " is het saldo: " + saldo;
     }
 
 }
