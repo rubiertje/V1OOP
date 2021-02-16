a90a87d27f5c10241b710c0cee3f6bd481c811b2
ruben <rubenvanrooijen23@outlook.com>
2021-02-16 13:02:12 +0100

    practicum 4b

--- test/practicum4/b/AutoHuurTest.java
+++ test/practicum4/b/AutoHuurTest.java
@@ -5,6 +5,18 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 class AutoHuurTest {
+    /**
+     Er zijn in totaal 6 verschillende testgevallen.
+     De totaalprijs() functie wordt tijdens de toString() functie automatisch meegetest, omdat het in de string zit.
+     wanneer er geen klant is, is er ook geen korting.
+     er komt alleen een totaalprijs en aantal dagen wanneer er een klant en een auto is.
+     1. een huur zonder auto en zonder klant.
+     2. een huur met een auto, maar geen klant.
+     3. een huur zonder auto, maar met een klant met korting.
+     4. een huur zonder auto, maar met een kant zonder korting.
+     5. een huur met een auto en met een klant met korting.
+     6. een huur met een auto en met een klant zonder korting.
+     **/
     @Test
     public void toStringGeenAutoGeenKlant(){
         AutoHuur ah1 = new AutoHuur();
