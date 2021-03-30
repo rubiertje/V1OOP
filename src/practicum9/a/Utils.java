39a04a3ae515087b43e4facebb0c5b3a73e04015
ruben <rubenvanrooijen23@outlook.com>
2021-03-30 21:38:43 +0200

    Practicum 9A

--- src/practicum9/a/Utils.java
+++ src/practicum9/a/Utils.java
@@ -1,15 +1,18 @@
 package practicum9.a;
 
+import java.util.Optional;
+import java.util.OptionalInt;
+
 public class Utils {
     private static int aantalAangeroepen=0;
 
-    public static String euroBedrag ( double bedrag){
-        aantalAangeroepen ++;
-        return String.format("%.2f", bedrag);
-    }
-    public static String euroBedrag ( double bedrag, int precisie){
+    public static String euroBedrag (double bedrag, int precisie){
         aantalAangeroepen ++;
         return String.format("%." + precisie + "f", bedrag);
+    }
+
+    public static String euroBedrag ( double bedrag){
+        return euroBedrag(bedrag,2);
     }
 
     public static int getAantalAangeroepen(){
